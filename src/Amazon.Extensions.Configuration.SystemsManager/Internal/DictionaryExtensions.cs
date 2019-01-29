d9736237cbc4e4a275c81f7ec4ea187cb3f03f67
Ken Hundley <kenhundley@gmail.com>
2019-01-29 16:59:41 -0500

    Moved the Dictionary compare into an Extension method and added tests

---
+++ src/Amazon.Extensions.Configuration.SystemsManager/Internal/DictionaryExtensions.cs
@@ -0,0 +1,40 @@
+﻿/*
+ * Copyright 2019 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License").
+ * You may not use this file except in compliance with the License.
+ * A copy of the License is located at
+ * 
+ *  http://aws.amazon.com/apache2.0
+ * 
+ * or in the "license" file accompanying this file. This file is distributed
+ * on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+
+using System.Collections.Generic;
+
+namespace Amazon.Extensions.Configuration.SystemsManager.Internal
+{
+    public static class DictionaryExtensions
+    {
+        public static bool DictionaryEqual<TKey, TValue>(this IDictionary<TKey, TValue> first, IDictionary<TKey, TValue> second) => DictionaryEqual(first, second, null);
+
+        public static bool DictionaryEqual<TKey, TValue>(this IDictionary<TKey, TValue> first, IDictionary<TKey, TValue> second, IEqualityComparer<TValue> valueComparer)
+        {
+            if (first == second) return true;
+            if (first == null || second == null) return false;
+            if (first.Count != second.Count) return false;
+
+            valueComparer = valueComparer ?? EqualityComparer<TValue>.Default;
+
+            foreach (var kvp in first)
+            {
+                if (!second.TryGetValue(kvp.Key, out var secondValue)) return false;
+                if (!valueComparer.Equals(kvp.Value, secondValue)) return false;
+            }
+            return true;
+        }
+    }
+}
---
+++ test/Amazon.Extensions.Configuration.SystemsManager.Tests/DictionaryExtensionsTests.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using Amazon.Extensions.Configuration.SystemsManager.Internal;
+using Xunit;
+
+namespace Amazon.Extensions.Configuration.SystemsManager.Tests
+{
+    public class DictionaryExtensionsTests
+    {
+        [Theory]
+        [MemberData(nameof(DictionaryEqualsData))]
+        public void TestDictionaryEquals(IDictionary<string, string> first, IDictionary<string, string> second, bool equals)
+        {
+            Assert.Equal(equals, first.DictionaryEqual(second));
+        }
+
+        public static TheoryData<IDictionary<string, string>, IDictionary<string, string>, bool> DictionaryEqualsData => new TheoryData<IDictionary<string, string>, IDictionary<string, string>, bool>
+        {
+            {new Dictionary<string, string>(), new Dictionary<string, string>(), true},
+            {new Dictionary<string, string>(), null, false},
+            {new Dictionary<string, string>(), new Dictionary<string, string> {{"a", "a"}}, false},
+            {new Dictionary<string, string> {{"a", "a"}}, new Dictionary<string, string> {{"a", "a"}}, true},
+            {new Dictionary<string, string> {{"a", "a"}}, new Dictionary<string, string> {{"a", "a"}, {"b", "b"}}, false},
+            {new Dictionary<string, string> {{"a", "a"}}, new Dictionary<string, string> {{"b", "b"}}, false},
+            {new Dictionary<string, string> {{"a", "a"}}, new Dictionary<string, string> {{"a", "b"}}, false},
+            {new Dictionary<string, string> {{"a", "a"}}, new Dictionary<string, string> {{"b", "a"}}, false},
+            {new Dictionary<string, string> {{"a", "a"},{"b", "b"}}, new Dictionary<string, string> {{"b", "b"},{"a", "a"}}, true}
+        };
+    }
+}
